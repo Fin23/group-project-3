8d3666da6d7178b58066ab522ee590e51a71af7f
darican <jcastillo8150@gmail.com>
2019-10-19 09:33:49 -0500

    adding WebScrape.js with basic CLI scrape from Indeed.com

---
+++ src/webScrape.js
@@ -0,0 +1,49 @@
+var cheerio = require("cheerio");
+var axios = require("axios");
+
+// First, tell the console what server3.js is doing
+console.log("\n******************************************\n" +
+                  "Webscraping through Indeed.com\n"+
+            "\n******************************************\n");
+
+// Make request via axios to grab the HTML from Indeeds website
+axios.get("https://www.indeed.com/jobs?q=junior+web+developer&l=Independence%2C+MO").then(function(response) {
+
+  // Load the HTML into cheerio
+  var $ = cheerio.load(response.data);
+
+  // Make an empty array for saving our scraped info
+  var results = [];
+
+//Use Cheerio and search through each div with class of "result"
+  $("div.result").each(function(i, element) {
+
+//Use Cheerio find method to gather job data info
+      
+      let jobTitle = $(element).find("div.title").find("a").attr("title");
+      let companyArray = $(element).find("div.sjcl").find("span.company").text().split(" ");
+      let company = companyArray.slice(-1)[0]; 
+      let city = $(element).find("div.sjcl").find("span.location").text().split(",")[0];
+      let state = $(element).find("div.sjcl").find("span.location").text().split(",")[1];
+      let summary = $(element).find("div.summary").find("b");
+          
+          
+          
+
+
+     
+
+//Push the Job main info into results array
+      
+      results.push({
+          jobTitle: jobTitle,
+          company: company,
+          city: city,
+          state: state,
+          summary: summary
+      });
+  });
+
+  // After looping through each element found, log the results to the console
+  console.log(results);
+});
