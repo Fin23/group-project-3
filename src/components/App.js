0bab8342719010cd1f397cf35d6b8198a54a5336
Sam Tricamo <samualtricamo@gmail.com>
2019-11-02 12:52:49 -0500

    moved oauth, working properly

--- src/components/App.js
+++ src/components/App.js
@@ -11,27 +11,31 @@ import '../css/App.css';
 class App extends React.Component {
   render (){
     return (
+      
       <div className="App">
+        
         <NavBar />
+        
         <header className="App-header">
+        
           <img src={pancake} className="App-logo" alt="pancake" />
           <p>
             Welcome to StaKC, the  one stop shop for new developers trying to break into the industry in Kansas City 
           </p>
+          <Login />
           <a className="App-link"
             href="https://reactjs.org"
             target="_blank"
             rel="noopener noreferrer">
-           Log In 
-          <br></br>
-          link still goes to react
+             
+           
           </a>
         </header>
 
       
 
 
-  <Login />
+  
  
 
 
