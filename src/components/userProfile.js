2aa16e987c118db2dd04048f5228f489bece3936
Ross Jameson <Rossjameson1623@gmail.com>
2019-11-04 10:50:56 -0600

    finising front end profile page

--- src/components/userProfile.js
+++ src/components/userProfile.js
@@ -1,121 +1,122 @@
 import React from 'react';
 import 'bootstrap/dist/css/bootstrap.min.css';
+import 'bootstrap/dist/js/bootstrap.bundle.min';
 import NavBar from './navbar';
 import '../css/userP.css';
 import '../css/grid.css';
+import link from '../images/link.png';
 import axios from 'axios';
+import $ from 'jquery';
+import Popper from 'popper.js';
 
-
-
-
-
-  
 
 class Profile extends React.Component {
 
     render(){
-   
       return (
         <div> 
-        
-        <NavBar />
-        {/* <div className="HP"> 
-        <header>Header: needs dynamic name</header>
-        <h1>Profiles</h1>
-        </div> */}
+          <NavBar />
+    
+    <div className="row">
 
-        <div className="row">
+        <div className="right-column-home" >
+          <div className="row1">
+            <div className="contact-home">
+              <div className="contactInfo"> 
+              
+                <div className="Name"> 
+                  Name: Link 
+                  </div>
+                <div className = "Email">
+                  Email: OcarianMaster23@gmail.com 
+                  </div>
+                <div className="Name">
+                  Phone Number: (481)-516-2342 
+                  </div>
+                <a href="https://github.com/Fin23/group-project-3">  
+                  <section className="icon-list">
+                    <i className="nes-icon github is-large"></i>
+                      <i className="nes-icon linkedin is-large"></i>
+                        <i className="nes-icon instagram is-large"></i>
+                          <i className="nes-icon gmail is-large"></i>
+                            </section>
+                            </a>
+                            </div>
+                            </div>
+                            </div>
 
-        <div className="right-column-home">
-          <div className="hero-image-container">
-            
-
-            <div className="pic"></div>
-            <input 
-            style={{display: 'none'}} 
-            type="file"  
-            onChange={this.fileSelectedHandler} 
-            ref={fileInput => this.fileInput = fileInput}/>
-            <button onClick={()=>this.fileInput.click()}>Pick File</button>
-            <button onClick={this.fileUploadHandler}>Upload</button>
-          </div>
-          <div className="aboutYou">Paragraph introduction about you</div>
-          <div className="traits">three favorite traits</div>
-
-          <div className="contactInfo"> 
-          <div>age: </div>
-           <div>Current Occupation: </div> 
-           <div>email:</div>
-           <div>phone number:</div>
-           <div>address:</div>
-           <div>email:</div>
-           <div>email:</div>
-           </div>
-
-</div>
-        
-
-        {/* <div className="middle-column-home">
-        <div className="workExperience">
-          <h5>work experience</h5>
-            <ol>
-              <li>job 1</li>
-              <li>job 2</li>
-              <li>job 3</li>
-              <li>job 4</li>
-            </ol>
-          </div>
-          <div className="Education">
-          <h5>Education</h5>
-            <ol>
-              <li>school 1</li>
-              <li>school 2</li>
-              <li>school 3</li>
-            </ol>
-            </div>
-          <div className="gitHubLink">GIT HUB LINK
-          <br></br>
-          <a href="#">
-  </a>
-  <section class="icon-list"><i className="nes-icon github is-large"></i></section>
-  
-          </div>
-
-          <div className="skills"> 
-          <h5>Skills</h5>
-          <div>JavaScript </div>
-           <div>React </div> 
-           <div>MySql</div>
-           <div>HTML</div>
-           <div>CSS</div>
-           <div>MongoDB</div>
-           <div>Git</div>
-           <div>PHP</div>
-           
-         
-           
-           </div>
-</div> */}
-{/* end of middle column */}
-       
-        
-         {/* <div className="left-column-home">
         <div className="hero-image-container">
-            <h3>/photo upload spot/</h3>
-          </div>
-          <div className="aboutYou">Paragraph introduction about you</div>
-          <div className="traits">three favorite traits</div>
-          <div className="contactInfo"> 
-          <div>age: </div>
-           <div>Current Occupation: </div> 
-           <div>email:</div>
-           <div>phone number:</div>
-           <div>address:</div>
-           <div>email:</div>
-           <div>email:</div>
-           </div>
-            </div>  */}
+          <div className="pic">
+            <img src={link} className="profilePic" alt="profilePic" />
+            </div>
+          <input 
+            style={{display: 'none'}} 
+              type="file"  
+                onChange={this.fileSelectedHandler} 
+                  ref={fileInput => this.fileInput = fileInput}/>
+                    <button onClick={()=>this.fileInput.click()}>
+                      Pick File
+                    </button>
+                  <button onClick={this.fileUploadHandler}>
+                    Upload
+                    </button>
+            </div>
 
+{/* paragraph button dropdown  */}
+<button className="item dropdown">
+  <p className="link dropdown-toggle" id="Dropdown" role="button" data-toggle="dropdown" aria-haspopup="true" aria-expanded="false">
+    Paragraph Introduction
+      </p>
+        <div className="dropdown-menu" aria-labelledby="Dropdown">
+          <p className="list">Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do eiusmod tempor incididunt ut labore et dolore magna aliqua. Ut enim ad minim veniam, quis nostrud exercitation ullamco laboris nisi ut aliquip ex ea commodo consequat. Duis aute irure dolor in reprehenderit in voluptate velit esse cillum dolore eu fugiat nulla pariatur. Excepteur sint occaecat cupidatat non proident, sunt in culpa qui officia deserunt mollit anim id est laborum.
+            <label for="textarea_field">
+              Textarea 
+                </label>
+              <textarea id="textarea_field" className="nes-textarea">
+                Edit Paragraph
+                  </textarea>
+                  </p>
+                  </div>
+                  </button>
+
+{/* education button drop down */}
+<button className="item dropdown">
+  <p className="link dropdown-toggle" id="Dropdown" role="button" data-toggle="dropdown" aria-haspopup="true" aria-expanded="false">
+    Education
+      </p>
+        <div className="dropdown-menu" aria-labelledby="Dropdown">
+          <p className="list">Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do eiusmod tempor incididunt ut labore et dolore magna aliqua. Ut enim ad minim veniam, quis nostrud exercitation ullamco laboris nisi ut aliquip ex ea commodo consequat. Duis aute irure dolor in reprehenderit in voluptate velit esse cillum dolore eu fugiat nulla pariatur. Excepteur sint occaecat cupidatat non proident, sunt in culpa qui officia deserunt mollit anim id est laborum.
+            <label for="textarea_field">
+              Textarea
+                </label>
+              <textarea id="textarea_field" className="nes-textarea">Add Certifications and or Schools attended</textarea>
+                </p>
+                </div>
+                </button>
+
+{/* skills button drop down */}
+<button className="item dropdown">
+  <p className="link dropdown-toggle" id="Dropdown" role="button" data-toggle="dropdown" aria-haspopup="true" aria-expanded="false">
+    Skills
+      </p>
+      <div className="dropdown-menu" aria-labelledby="Dropdown">
+        <p className="list">
+          Javascript:50%
+            <progress className="nes-progress is-success" value="50" max="100">
+            </progress>
+              React:80%
+                <progress className="nes-progress is-success" value="80" max="100">
+                </progress>
+                  MongoDB:70%
+                  <progress className="nes-progress is-success" value="70" max="100">
+                    </progress>
+                    </p>
+                    </div>
+                    </button>
+      
+{/* end of right column home div */}
+</div>
+{/* below is the end tag for row div */}
         </div>
 
 
