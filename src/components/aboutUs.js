752a757fd9ad3ea6b521a49e007220b4c6c37a6c
Sam Tricamo <samualtricamo@gmail.com>
2019-11-14 16:47:29 -0600

    aboutus moved

--- src/components/aboutUs.js
+++ src/components/aboutUs.js
@@ -7,14 +7,20 @@ import '../css/aboutUs.css';
 
 
 
+
+
 class Profile extends React.Component {
 
     render(){
       return (
         <div> 
           <NavBar />
-    
-    <div className="row">
+        
+          <div className="about-App">
+        <header className="about-header">
+          
+        <div className="row">
+         
         <div className="about-right-column-home" >
           <div className="about-row1">
             <div className="about-contact-home">
@@ -29,11 +35,15 @@ class Profile extends React.Component {
   
 </div>
        
-
+         
         <div className="about-hero-image-container">
         <div className="about-pic">
+        <i class="nes-mario character"></i>
+      
+        </div>
         <section className="aboutIcon">
-        <div className="Name"> 
+        <div className="about-Name"> 
+        <br></br>
                   Name:Reese Borchers
                   </div>
         <a href="https://github.com/FatherOfTheSun"> <i className="nes-icon github is-medium"></i> </a>
@@ -42,13 +52,17 @@ class Profile extends React.Component {
 
                           </section>
           
-            </div>
+            
             </div>
             
         <div className="about-hero-image-container">
         <div className="about-pic">
+        <i class="nes-ash"></i>
+
+        </div>
         <section className="aboutIcon">
-        <div className="Name"> 
+          <br></br>
+        <div className="about-Name"> 
                   Name:Joe Castillo
                   </div>
         <a href="https://github.com/DaRican"> <i className="nes-icon github is-medium"></i> </a>
@@ -56,39 +70,48 @@ class Profile extends React.Component {
         <a href="https://mail.google.com/mail/?view=cm&fs=1&to=jcastillo8150@gmail.com&su=SUBJECT&body=BODY"> <i className="nes-icon gmail is-medium"> </i></a>
 
                           </section>
-            </div>
+           
             </div>
 
         <div className="about-hero-image-container">
         <div className="about-pic">
+        <i class="nes-bulbasaur character"></i>
+        </div>
         <section className="aboutIcon">
-        <div className="Name"> 
+        <br></br>
+        <div className="about-Name"> 
                   Name:Ross Jameson
                   </div>
         <a href="https://github.com/Fin23"> <i className="nes-icon github is-medium"></i> </a>
         <a href="https://www.linkedin.com/in/ross-jameson-891047186/"> <i className="nes-icon linkedin is-medium"> </i> </a>
         <a href="https://mail.google.com/mail/?view=cm&fs=1&to=rossjameson1623@gmail.com&su=SUBJECT&body=BODY"> <i className="nes-icon gmail is-medium"> </i></a>
                      </section>
-            </div>
+           
             </div>
 
         <div className="about-hero-image-container">
         <div className="about-pic">
+        <i class="nes-squirtle character"></i>
+        </div>
         <section className="aboutIcon">
-        <div className="Name"> 
+        <div className="about-Name"> 
+        <br></br>
                   Name:Sam Tricamo 
                   </div>
         <a href="https://github.com/samualtricamo"> <i className="nes-icon github is-medium"></i> </a>
         <a href="https://www.linkedin.com/in/sam-tricamo-40480150/"> <i className="nes-icon linkedin is-medium"> </i> </a>
         <a href="https://mail.google.com/mail/?view=cm&fs=1&to=samualtricamo@gmail.com&su=SUBJECT&body=BODY"> <i className="nes-icon gmail is-medium"> </i></a>
                           </section>
-            </div>
+          
             </div>
             
         <div className="about-hero-image-container">
         <div className="about-pic">
+        <i class="nes-charmander character"></i>
+        </div>
         <section className="aboutIcon">
-        <div className="Name"> 
+        <div className="about-Name"> 
+        <br></br>
                   Name:Peter Walker
                   </div>
         <a href="https://github.com/peteawalk"> <i className="nes-icon github is-medium"></i> </a>
@@ -96,33 +119,48 @@ class Profile extends React.Component {
         <a href="https://mail.google.com/mail/?view=cm&fs=1&to=pw0107@gmail.com&su=SUBJECT&body=BODY"> <i className="nes-icon gmail is-medium"> </i></a>
                  </section>
             </div>
-            </div>
+          
 
         <div className="about-hero-image-container">
         <div className="about-pic">
+        <i class="nes-kirby character"></i>
+        </div>
         <section className="aboutIcon">
-        <div className="Name"> 
-                  Name:You! We Are Hiring!
+        <div className="about-Name"> 
+        <br></br>
+                  Name:You!
                   </div>
         <a href=""> <i className="nes-icon github is-medium"></i> </a>
         <a href=""> <i className="nes-icon linkedin is-medium"> </i> </a>
         <a href=""> <i className="nes-icon gmail is-medium"> </i></a>
                  </section>
             </div>
-            </div>
+           
       
 {/* end of right column home div */}
 </div>
 {/* below is the end tag for row div */}
-        </div>
+
+             
+         
+
+         
+      
+        </header>
+
+      
+
+
+  
+ 
+
+
+      </div>
 
 
 
 
-       
-
-        
-        
+</div>
       )
      
     }
